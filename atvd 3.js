1fea6fba3fd399f1cb623c9402b32a35ebf6e218
Vitor Lima <vitorlima21.dev@gmail.com>
2026-08-29 09:01:16 -0300

    Atividade 3

---
+++ atvd 3.js
@@ -0,0 +1,14 @@
+const nota1 = 7.4
+const nota2 = 8.9
+const nota3 = 8.4
+
+// nota 1 - peso 2 
+// nota 2 - peso 3 
+// nota 3 - peso 5 
+
+const media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / (2 + 3 + 5)
+
+console.log("A média ponderada é: ", media)
+
+
+
