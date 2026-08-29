ce00dd789a044709c046bea48df5ae58a2f2bab1
Vitor Lima <vitorlima21.dev@gmail.com>
2026-08-29 08:23:42 -0300

    Atividade 1

---
+++ atvd 1.js
@@ -0,0 +1,5 @@
+const produto = 250
+const percentual = 0.15  // = 15% (15/100)
+const valor = (produto * percentual)
+
+console.log("O valor do produto com o desconto é: ", valor.toFixed(2));  // O .toFixed(2) transforma o número em texto com 2 casas decimais
